e0cecd84a2978d2f1578e108de8b233e4b20771a
Jon Homan <jon@jonhoman.com>
2012-02-20 20:12:05 -0600

    let it be

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,8 +7,8 @@ describe Board do
     lambda { Board.parse( [ ([nil] * 9).flatten] * 9) }.should_not raise_error
   end
 
-  it "should have rows" do 
-    board = Board.parse([
+  let(:board) do
+    Board.parse([
       [1, 2, 3, 5, 4, 6, 7, 8, 9],
       [1, 2, 3, 4, 5, 6, 7, 8, 9],
       [1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -18,37 +18,20 @@ describe Board do
       [1, 2, 3, 4, 5, 6, 7, 8, 9],
       [1, 2, 3, 4, 5, 6, 7, 8, 9],
       [1, 2, 3, 4, 5, 6, 7, 8, 9]])
+  end
+
+  it "should have rows" do 
     board.should_not be_nil
     board.row(0).should == [1, 2, 3, 5, 4, 6, 7, 8, 9]
     board.row(1).should == [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
   it "should have columns" do
-    board = Board.parse([
-      [1, 2, 3, 5, 4, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9]])
     board.columns(0).should == [1] * 9
     board.columns(1).should == [2] * 9
   end
   
   it "should have squares" do
-    board = Board.parse([
-      [1, 2, 3, 5, 4, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9]])
     board.square(0).should == [1, 2, 3, 1, 2, 3, 1, 2, 3]
   end
 
