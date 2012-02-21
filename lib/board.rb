48e40a8b81911c71bd9f6b18bd90b02947099f4e
Brian Pratt <brian@8thlight.com>
2012-02-20 19:04:43 -0600

    Updated with encode

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,16 @@ class Board
   def self.parse(grid)
     return new
   end
+
+  def encoded
+    return ""
+  end
+end
+
+class SudokuSolver
+  def solve(board)
+    return board
+  end
 end
 
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,5 +5,17 @@ describe Board do
     it "returns an instance of Board" do
       Board.parse([]).should be_a Board
     end
+
+    it "sudokusolver can take a board" do
+      board = Board.parse([])
+      solver = SudokuSolver.new
+      solved = solver.solve(board)
+      solved.should be_a Board
+    end
+
+    it "encoded returns a stirng" do
+      board = Board.parse([])
+      board.encoded.should be_a String
+    end
   end
 end
