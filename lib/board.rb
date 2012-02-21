7d7a9e3326c13d05517d74be8945590572f87c7a
Jon Homan <jon@jonhoman.com>
2012-02-20 19:05:57 -0600

    second test

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,10 @@
 class Board
   def self.parse(what)
+    self.new
+  end
+
+  def row(index)
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 end
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,17 +4,22 @@ require 'board'
 describe Board do
   
   it "should parse" do
-    lambda { Board.parse(
-      [
-        [5,3,nil,nil,7,nil,nil,nil,nil],
-        [6,nil,nil,1,9,5,nil,nil,nil],
-        [nil,9,8,nil,nil,nil,nil,6,nil],
-        [8,nil,nil,nil,6,nil,nil,nil,3],
-        [4,nil,nil,8,nil,3,nil,nil,1],
-        [7,nil,nil,nil,2,nil,nil,nil,6],
-        [nil,6,nil,nil,nil,nil,2,8,nil],
-        [nil,nil,nil,4,1,9,nil,nil,5],
-        [nil,nil,nil,nil,8,nil,nil,7,9]
-    ]) }.should_not raise_error
+    lambda { Board.parse( [ ([nil] * 9).flatten] * 9) }.should_not raise_error
   end
+
+  it "should have columns" do 
+    board = Board.parse([
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9]])
+      board.should_not be_nil
+      board.row(0).should == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
 end
