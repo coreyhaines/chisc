22864a790e1173b65a66db38871e2a914d4bb19d
Jon Homan <jon@jonhoman.com>
2012-02-20 19:32:27 -0600

    next step

--- lib/sudoku_solver/board.rb
+++ lib/sudoku_solver/board.rb
@@ -2,4 +2,10 @@ class Board
   def self.parse(raw_board)
     new
   end
+  
+  def possibilities(row, column)
+    [1,2,3,4,5,6,7,8,9]
+  end
 end
+
+
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,5 +7,10 @@ describe Board do
     it 'returns a board' do
       Board.parse(raw_board).should be_a_kind_of Board
     end
+
+    it "should have possibilities" do
+      Board.parse(([nil] *9) * 9).possibilities(0,0).should == [1,2,3,4,5,6,7,8,9]
+    end
+
   end
 end
