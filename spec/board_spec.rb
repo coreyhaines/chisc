4649fa1cbb4c134e5cb856d84371aba1c22631e7
Emilio Gutter <egutter@10pines.com>
2012-02-20 19:26:30 -0600

    Removed crap and aded complete?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,13 +2,19 @@ require 'spec_helper'
 
 class Board
   def self.parse(board)
-    board
+    Board.new(board)
   end
+
+  def complete?
+    false
+  end
+
 end
 describe Board do
-  it "returns board" do
-    board = [1, 2, 3]
 
-    Board.parse(board).should == board
+  it "board is incomplete with a nil" do
+    board = [1, 2, nil]
+
+    Board.parse(board).complete?.should be_false
   end
 end
