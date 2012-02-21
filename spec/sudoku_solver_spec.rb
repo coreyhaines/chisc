ce9f91a510dea71d553c34adbc45226ece8927f1
Tim Ottinger <tottinge@gmail.com>
2012-02-20 19:36:32 -0600

    whew

--- spec/sudoku_solver_spec.rb
+++ spec/sudoku_solver_spec.rb
@@ -4,12 +4,12 @@ require 'spec_helper.rb'
 class SudokuSolver; end
 
 class Board 
-  def parse ary; 
+  def self.parse ary; 
   end
 end
 
 describe SudokuSolver do
   it "should be able to parse a board" do
-     Board.new.parse([])
+     Board.parse([])
   end
 end 
