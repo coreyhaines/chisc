b78176151224ca4206f9767066e3984d40a14125
Jon Homan <jon@jonhoman.com>
2012-02-20 19:42:44 -0600

    next test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,13 +8,10 @@ describe Board do
       Board.parse(raw_board).should be_a_kind_of Board
     end
 
-    it "should have possibilities" do
-      Board.parse(([nil] *9) * 9).possibilities(0,0).should == [1,2,3,4,5,6,7,8,9]
-    end
 
     it "should eliminate possibilities from the same row" do
       Board.parse([
-                  [nil, nil, nil, nil, nil, nil, nil, nil, 1],
+                  [nil, nil, nil, nil, nil, nil, nil, nil, nil],
                   [nil, nil, nil, nil, nil, nil, nil, nil, nil],
                   [nil, nil, nil, nil, nil, nil, nil, nil, nil],
                   [nil, nil, nil, nil, nil, nil, nil, nil, nil],
@@ -23,7 +20,7 @@ describe Board do
                   [nil, nil, nil, nil, nil, nil, nil, nil, nil],
                   [nil, nil, nil, nil, nil, nil, nil, nil, nil],
                   [nil, nil, nil, nil, nil, nil, nil, nil, nil]
-      ]).possibilities(0, 0).should == [2, 3, 4, 5, 6, 7, 8, 9]
+      ]).possibilities(0, 0).should == [1, 2, 3, 4, 5, 6, 7, 8, 9]
     end
 
   end
