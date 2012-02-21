23cc28f72d1790b95799e3ab2ae5fd7f8f3cf024
Josh Cheek <josh.cheek@gmail.com>
2012-02-20 20:07:29 -0600

    Refactor tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,7 +15,9 @@ EMPTY_BOARD = [
 EMPTY_BOARD.extend(DeepClone)
 describe Board do
   describe '.parse' do
-    let(:raw_board) { COMPLETE_BOARD.deep_clone }
+    let(:raw_board)       { COMPLETE_BOARD.deep_clone }
+    let(:raw_empty_board) { EMPTY_BOARD.deep_clone }
+    let(:empty_board)     { Board.parse raw_empty_board }
     
     it 'returns a board' do
       Board.parse(raw_board).should be_a_kind_of Board
@@ -36,23 +38,20 @@ describe Board do
     end
 
     it "should eliminate multiple possibilities in the same row" do
-      board = EMPTY_BOARD.deep_clone
-      board[0][8] = 1
-      board[0][3] = 9
-      Board.parse(board).possibilities(0, 0).should == [2, 3, 4, 5, 6, 7, 8]
+      raw_empty_board[0][8] = 1
+      raw_empty_board[0][3] = 9
+      empty_board.possibilities(0, 0).should == [2, 3, 4, 5, 6, 7, 8]
     end
 
     it "should eliminate possibilities in the same column" do
-      board = EMPTY_BOARD.deep_clone
-      board[7][0] = 1
-      Board.parse(board).possibilities(0, 0).should == [2, 3, 4, 5, 6, 7, 8, 9]
+      raw_empty_board[7][0] = 1
+      empty_board.possibilities(0, 0).should == [2, 3, 4, 5, 6, 7, 8, 9]
     end
 
     it "should eliminate possibilities in the same row same column" do
-      board = EMPTY_BOARD.deep_clone
-      board[7][0] = 1
-      board[0][1] = 5
-      Board.parse(board).possibilities(0, 0).should == [2, 3, 4, 6, 7, 8, 9]
+      raw_empty_board[7][0] = 1
+      raw_empty_board[0][1] = 5
+      empty_board.possibilities(0, 0).should == [2, 3, 4, 6, 7, 8, 9]
     end
   end
 end
