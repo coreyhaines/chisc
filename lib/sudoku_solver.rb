185bf904ceea5ef7f7478e21ca28054ba5a98ace
Josh Cheek <josh.cheek@gmail.com>
2012-02-20 19:54:15 -0600

    Board#[]

--- lib/sudoku_solver.rb
+++ lib/sudoku_solver.rb
@@ -11,4 +11,9 @@ class Board
   def self.parse(raw_board)
     Board.new
   end
+  
+  def [](x,y)
+    0
+  end
+  
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,4 +5,9 @@ describe Board do
   specify '.parse returns a new board' do
     Board.parse(raw_board).should be_a_kind_of Board
   end
+  
+  let(:incremental_board) { (0...81).each_slice(9).to_a }
+  it 'grants x,y access to its coordinates' do
+    Board.parse(incremental_board)[0, 0].should be_zero
+  end
 end
