5a28b6774bdf596fc549f15087053935552fea40
Tim Ottinger <tottinge@gmail.com>
2012-02-20 19:13:07 -0600

    okay

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,9 +3,13 @@
 require 'spec_helper.rb'
 
 class SudokoSolver
+  def initialize(board)
+  end
+end
 
 describe SudokoSolver
   it "does something"
-     SudokoSolver.new
+
+     SudokoSolver.new(COMPLETEBOARD)
   end
 end
